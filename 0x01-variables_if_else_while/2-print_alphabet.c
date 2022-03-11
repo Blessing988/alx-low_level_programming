e1eae9b5e7be4ac34267b7324b5888cdc6824d8c
Blessing Agyei Kyem <blessing@DESKTOP-310NQCC.localdomain>
2022-03-11 14:08:48 +0100

    displaying all lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char x;
+
+	for (x='a'; x <= 'z'; x++)
+    {
+        putchar(x);
+    }
+    
+    return (0);
+}
